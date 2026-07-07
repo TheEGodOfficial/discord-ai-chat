c6410bbad316e0afe75396736956f243692084ea
TheEGodOfficial <fredrickgod35@gmail.com>
2026-07-07 06:46:43 -0400

    fix overlooked problem that was funny af

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,4 +1,4 @@
-import WorkspacePageClient from "../_components/WorkspacePageClient"
+import WorkspacePageClient from "../_components/WorkspacePageClient.tsx"
 
 export default function WorkspacePage() {
   return <WorkspacePageClient />
